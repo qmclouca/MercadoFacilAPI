5e10ff0acf37c854108a78a24127394dcd7dd2e8
qmclouca <qmclouca@gmail.com>
2023-12-18 23:36:10 -0300

    Criando CrossCutting

---
+++ CrossCutting/DependencyInjection/ConfigureServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CrossCutting.DependencyInjection.DependencyInjection
+{
+    public class ConfigureServices
+    {
+        public static void ConfigureDependenciesServices (IServiceCollection serviceCollection)
+        {
+            //serviceCollection.AddTransient<IUserService, UserService>();
+        }
+    }
+}
