6a98103d6aa41d4e262148ab7c38b0bcabe47d28
qmclouca <qmclouca@gmail.com>
2023-12-23 12:57:18 -0300

    ajustando BaseEntity

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -2,6 +2,7 @@
 {
     public class User : BaseEntity
     {
+        public string? Name { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
         public string? Role { get; set; }
