24f33bccdab928bf88c48cf53acb70b0a17b51a2
qmclouca <qmclouca@gmail.com>
2023-12-29 23:46:47 -0300

    corrigindo mapeamento user, CreateUserDTO

---
+++ Data/Migrations/20231230022347_regularizar.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class regularizar : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Addresses",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_UserId",
+                table: "Addresses",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Addresses_Users_UserId",
+                table: "Addresses",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Addresses_Users_UserId",
+                table: "Addresses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Addresses_UserId",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Addresses");
+        }
+    }
+}
--- Domain/Services/UserService.cs
+++ Domain/Services/UserService.cs
@@ -40,10 +40,14 @@ namespace Domain.Services
         public async Task<User> GetUserById(Guid id)
         {
             User user = await _userRepository.GetByIdAsync(id);
+            
+            //61460d39-fcdf-49e9-b9f9-45b34c46be48
+
+
             IEnumerable<UserAddress> userAddresses = await _userAddressRepository.GetAllAsync();
             IEnumerable<UserAddress> userAddressesFiltered = userAddresses.Where(ua => ua.UserId == id);                        
             List<Address> addresses = new List<Address>();
-            foreach (UserAddress userAddress in userAddresses)
+            foreach (UserAddress userAddress in userAddressesFiltered)
             {
                 Address address = await _addressRepository.GetByIdAsync(userAddress.AddressId);
                 
