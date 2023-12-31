01490f053b7649701ba6555974314e03b15262af
qmclouca <qmclouca@gmail.com>
2023-12-31 20:32:11 -0300

    refatorando para deixar os métodos melhores

--- MercadoFacilAPI/Controllers/UserController.cs
+++ MercadoFacilAPI/Controllers/UserController.cs
@@ -1,10 +1,9 @@
 ﻿using AutoMapper;
+using Domain.DTOs.Address;
 using Domain.DTOs.User;
 using Domain.Entities;
 using Domain.Interfaces.Services;
-using Newtonsoft.Json;
 using Microsoft.AspNetCore.Mvc;
-using Domain.DTOs.Address;
 
 namespace MercadoFacilAPI.Controllers
 {
@@ -53,78 +52,10 @@ namespace MercadoFacilAPI.Controllers
 
             if (userDto == null)
                 return BadRequest();
-
-            User user = new User();
-            List<Address> lstAddress = new List<Address>();
-            List<UserAddress> lstUserAddress = new List<UserAddress>();
-
-            _mapper.Map(userDto, user);
-            if(userDto.Addresses != null)
-            {
-                foreach (var item in userDto.Addresses)
-                {
-                    Address addressItem = new Address();
-                    UserAddress userAddress = new UserAddress();
-
-                    addressItem.Id = Guid.NewGuid();
-                    addressItem.IsDeleted = false;
-                    addressItem.Active = true;
-                    addressItem.Number = item.Number;
-                    addressItem.Street = item.Street;
-                    addressItem.Complement = item.Complement;
-                    addressItem.Neighborhood = item.Neighborhood;
-                    addressItem.City = item.City;
-                    addressItem.State = item.State;
-                    addressItem.Country = item.Country;
-                    addressItem.ZipCode = item.ZipCode;
-                    addressItem.District = item.District;
-                    userAddress.Id = Guid.NewGuid();
-                    userAddress.UserId = user.Id;
-                    userAddress.AddressId = addressItem.Id;
-                    userAddress.Active = true;
-                    userAddress.IsDeleted = false;
-                    lstAddress.Add(addressItem);
-                    lstUserAddress.Add(userAddress);
-                }
-            }                     
+            User user = await ConvertCreateUserDTOToUser(userDto);
 
             await _userService.AddUser(user);
-                       
-            foreach (var item in lstAddress)
-            {
-                await _addressService.AddAddress(item);               
-            }
-            foreach (var item in lstUserAddress)
-            {
-                await _userAddressService.AddUserAddress(item); 
-            }
-
-            userDto = new CreateUserDTO();
-
-            userDto.Name = user.Name;
-            userDto.Email = user.Email;
-            userDto.Password = user.Password;
-            userDto.Role = user.Role;
-            List<CreateAddressDTO> lstCreateAddressDTO = new List<CreateAddressDTO>();
-            if (lstAddress.Count > 0)
-            {
-                foreach (var item in lstAddress)
-                {
-                    CreateAddressDTO createAddressDTO = new CreateAddressDTO();
-                    createAddressDTO.Number = item.Number;
-                    createAddressDTO.Street = item.Street;
-                    createAddressDTO.Complement = item.Complement;
-                    createAddressDTO.Neighborhood = item.Neighborhood;
-                    createAddressDTO.City = item.City;
-                    createAddressDTO.State = item.State;
-                    createAddressDTO.Country = item.Country;
-                    createAddressDTO.ZipCode = item.ZipCode;
-                    createAddressDTO.District = item.District;
-                    lstCreateAddressDTO.Add(createAddressDTO);
-                }
-            }
-            userDto.Addresses = lstCreateAddressDTO;
-            return Ok(userDto);
+            return Ok(ConvertUserToCreateUserDTO(user));
         }
 
         [HttpPut(Name = "UpdateUser")]
@@ -150,5 +81,107 @@ namespace MercadoFacilAPI.Controllers
             
             return Ok(user);
         }
+
+        #region métodos auxiliares
+        private async Task<User> ConvertCreateUserDTOToUser(CreateUserDTO createUserDTO)
+        {
+            User user = new User();
+            List<Address> lstAddress = new List<Address>();
+            List<UserAddress> lstUserAddress = new List<UserAddress>();
+
+            _mapper.Map(createUserDTO, user);
+            if (createUserDTO.Addresses != null)
+            {
+                foreach (var item in createUserDTO.Addresses)
+                {
+                    Address addressItem = new Address();
+                    UserAddress userAddress = new UserAddress();
+
+                    addressItem.Id = Guid.NewGuid();
+                    addressItem.IsDeleted = false;
+                    addressItem.Active = true;
+                    addressItem.Number = item.Number;
+                    addressItem.Street = item.Street;
+                    addressItem.Complement = item.Complement;
+                    addressItem.Neighborhood = item.Neighborhood;
+                    addressItem.City = item.City;
+                    addressItem.State = item.State;
+                    addressItem.Country = item.Country;
+                    addressItem.ZipCode = item.ZipCode;
+                    addressItem.District = item.District;
+                    userAddress.Id = Guid.NewGuid();
+                    userAddress.UserId = user.Id;
+                    userAddress.AddressId = addressItem.Id;
+                    userAddress.Active = true;
+                    userAddress.IsDeleted = false;
+                    lstAddress.Add(addressItem);
+                    lstUserAddress.Add(userAddress);
+                }
+            }
+            await SaveAddressList(lstAddress);
+            await SaveUserAddressList(lstUserAddress);
+            return user;
+        }
+
+        private CreateUserDTO ConvertUserToCreateUserDTO(User user)
+        {
+            CreateUserDTO userDto = new CreateUserDTO();
+            userDto.Name = user.Name;
+            userDto.Email = user.Email;
+            userDto.Password = user.Password;
+            userDto.Role = user.Role;
+            List<CreateAddressDTO> lstCreateAddressDTO = new List<CreateAddressDTO>();
+            if (user.Addresses.Count > 0)
+            {
+                foreach (var item in user.Addresses)
+                {
+                    CreateAddressDTO createAddressDTO = new CreateAddressDTO();
+                    createAddressDTO.Number = item.Number;
+                    createAddressDTO.Street = item.Street;
+                    createAddressDTO.Complement = item.Complement;
+                    createAddressDTO.Neighborhood = item.Neighborhood;
+                    createAddressDTO.City = item.City;
+                    createAddressDTO.State = item.State;
+                    createAddressDTO.Country = item.Country;
+                    createAddressDTO.ZipCode = item.ZipCode;
+                    createAddressDTO.District = item.District;
+                    lstCreateAddressDTO.Add(createAddressDTO);
+                }
+            }
+            userDto.Addresses = lstCreateAddressDTO;
+            return userDto;
+        }
+
+        private async Task<bool> SaveAddressList(List<Address> lstAddresses)
+        {
+            try
+            {
+                foreach (var item in lstAddresses)
+                {
+                    await _addressService.AddAddress(item);
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        private async Task<bool> SaveUserAddressList(List<UserAddress> lstUserAddresses)
+        {
+            try
+            {
+                foreach (var item in lstUserAddresses)
+                {
+                    await _userAddressService.AddUserAddress(item);
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        #endregion
     }
 }
