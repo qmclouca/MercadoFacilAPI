3012238b55c35890dcce683bc7d7b58dcc6ff789
qmclouca <qmclouca@gmail.com>
2024-01-04 23:56:31 -0300

    teste email válido

--- UnitTests/UserControllerTests.cs
+++ UnitTests/UserControllerTests.cs
@@ -49,5 +49,34 @@ namespace UnitTests
             Assert.IsType<BadRequestResult>(result.Result);
         }
 
+        [Theory]
+        [InlineData("usuariosemarroba.com")]
+        [InlineData("usuariosem@ponto")]
+        [InlineData("usuariosemarrobaeponto")]
+        [InlineData("usuario@@exemplo.com")]
+        public async void Post_ReturnsBadRequest_WhenEmailIsInvalid(string email)
+        {
+            // Arrange
+            var mockUserService = new Mock<IUserService>();
+            var mockAddressService = new Mock<IAddressService>();
+            var mockUserAddressService = new Mock<IUserAddressService>();
+            var mockMapper = new Mock<IMapper>();
+                        
+            var userController = new UserController(mockUserService.Object, mockAddressService.Object, mockUserAddressService.Object, mockMapper.Object);
+            var userDto = new CreateUserDTO
+            {
+                Name = "Usuário Teste",
+                Email = email,
+                Password = "Senha#1234",
+                Role = "User"
+            };
+
+            // Act
+            var result = await userController.Post(userDto);            
+
+            // Assert            
+            var badRequestResult = result as BadRequestResult;
+            Assert.Equal(400, badRequestResult.StatusCode);
+        }
     }
 }
