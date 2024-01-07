2c07c4c0cba92ada8f202e601556b9ae5f417da2
qmclouca <qmclouca@gmail.com>
2024-01-07 11:31:28 -0300

    teste unitário para complexidade do password funcionando

--- UnitTests/UserControllerTests.cs
+++ UnitTests/UserControllerTests.cs
@@ -79,7 +79,7 @@ namespace UnitTests
         [InlineData("usuariosem@ponto")]
         [InlineData("usuariosemarrobaeponto")]
         [InlineData("usuario@@exemplo.com")]
-        public async void Post_ReturnsBadRequest_WhenEmailIsInvalid(string email)
+        public async Task Post_ReturnsBadRequest_WhenEmailIsInvalid(string email)
         {
             // Arrange
             var mockUserService = new Mock<IUserService>();
@@ -137,7 +137,7 @@ namespace UnitTests
         [InlineData("Inv@lid", false, "A senha deve conter pelo menos um número, um caractere especial, uma letra maiúscula e uma minúscula.")] // Inválido: sem número
         [InlineData("Short1!", false, "A senha deve conter no mínimo 12 caracteres e no máximo 40.")] // Inválido: menos de 12 caracteres
         [InlineData("ThisPasswordIsWayTooLongEvenIfItHasNumbers1!", false, "A senha deve conter no mínimo 12 caracteres e no máximo 40.")] // Inválido: mais de 40 caracteres
-        public async void Password_Validation_Test(string password, bool expectedIsValid, string expectedMessage)
+        public async Task Password_Validation_Test(string password, bool expectedIsValid, string expectedMessage)
         {
             // Arrange
             var mockUserService = new Mock<IUserService>();
@@ -158,8 +158,7 @@ namespace UnitTests
                 ZipCode = "12345678"
             };
             
-            List<CreateAddressDTO> lstCreateAddresses = new List<CreateAddressDTO>();
-            lstCreateAddresses.Add(address);
+            List<CreateAddressDTO> lstCreateAddresses = [address];
 
             var userDto = new CreateUserDTO
             {
@@ -171,6 +170,19 @@ namespace UnitTests
             };
 
             // Act
+            var context = new ValidationContext(userDto, null, null);
+            var results = new List<System.ComponentModel.DataAnnotations.ValidationResult>();
+
+            var isValid = Validator.TryValidateObject(userDto, context, results, true);
+
+            if(!isValid)
+            {
+                foreach (var validationResult in results)
+                {
+                    userController.ModelState.AddModelError(validationResult.MemberNames.First(), validationResult.ErrorMessage);
+                }
+            }
+
             var result = await userController.Post(userDto);
             var objectResult = result as ObjectResult;
 
@@ -183,9 +195,33 @@ namespace UnitTests
             else
             {
                 Assert.NotNull(result);
-                Assert.IsType<OkObjectResult>(result);
-                var valueResult = objectResult.Value as List<Bogus.ValidationResult>;
-                // Assert.Equal(expectedMessage, valueResult[].);
+                Assert.IsType<BadRequestObjectResult>(result);
+                
+                var errorMessages = new List<string>();
+                var resultValue = result as BadRequestObjectResult;
+                if (resultValue == null) { 
+                    Assert.True(false, "resultValue is null");
+                }
+                var modelState = resultValue.Value as SerializableError;
+
+                foreach (var KeyValuePair in modelState)
+                {
+                    var key = KeyValuePair.Key;
+                    var errors = KeyValuePair.Value as string[];
+
+                    if (errors != null && errors.Length > 0)
+                    {
+                        errorMessages.AddRange(errors);
+                    }
+                }
+
+                var expectedMessageOne = "A senha deve conter no mínimo 12 caracteres e no máximo 40.";
+                var expectedMessageTwo = "A senha deve conter pelo menos um número, um caractere especial, uma letra maiúscula e uma minúscula.";
+
+                bool containsEitherMessage = errorMessages.Any(msg => msg.Contains(expectedMessageOne)) ||
+                                             errorMessages.Any(msg => msg.Contains(expectedMessageTwo));
+
+                Assert.True(containsEitherMessage, "Error messages should contain either message one or message two.");
             }
         }
     }
