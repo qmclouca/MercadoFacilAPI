a000773c053ece2c0213a0e12cb4281fb92e9407
qmclouca <qmclouca@gmail.com>
2024-01-15 23:56:34 -0300

    modificando conversor user dto

---
+++ Domain/DTOs/UserConversionResultDTO.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+
+namespace Domain.DTOs
+{
+    public class UserConversionResultDTO
+    {
+        public Domain.Entities.User? user { get; set; }
+        public List<Domain.Entities.Address> Addresses { get; set; }
+        public List<UserAddress> UserAddresses { get; set; }
+
+        public UserConversionResultDTO()
+        {
+            Addresses = new List<Domain.Entities.Address>();
+            UserAddresses = new List<UserAddress>();
+        }
+    }
+}
