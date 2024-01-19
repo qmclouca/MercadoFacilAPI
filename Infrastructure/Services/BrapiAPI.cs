0cf6e52d5ed507a64698560202d24389b6ecc52a
qmclouca <qmclouca@gmail.com>
2024-01-18 23:57:30 -0300

    Brapi implementado

--- Infrastructure/Services/BrapiAPI.cs
+++ Infrastructure/Services/BrapiAPI.cs
@@ -1,14 +1,36 @@
-﻿namespace Infrastructure.Services
+﻿using Infrastructure.Interfaces;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+
+
+namespace Infrastructure.Services
 {
-    public class BrapiAPI
+    public class BrapiAPI: IBrapiService
     {
         private readonly string _brapiAPIKey;
         private readonly string _brapiBaseURL;
+        private HttpClient _client = new HttpClient();
 
-        public BrapiAPI()
+        public BrapiAPI(IOptions<ExternalAPIConfigurations> configurations)
         {
-            
+            _brapiAPIKey = configurations.Value.BRAPI_API_KEY;
+            _brapiBaseURL = configurations.Value.BRAPI_URL;
         }
 
+        public async Task<dynamic> FetchCompanyData(string symbol)
+        {
+            var url = $"{_brapiBaseURL}{symbol}?token={_brapiAPIKey}";
+            return await FetchUrl(url);
+        }
+
+        private async Task<dynamic> FetchUrl(string url)
+        {
+            var response = await _client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            string responseContent = await response.Content.ReadAsStringAsync();
+            
+            return JsonConvert.DeserializeObject(responseContent);
+        }
     }
 }
