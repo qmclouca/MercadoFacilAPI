56566741a1d624830b522c080b27cccdaa74adf7
qmclouca <qmclouca@gmail.com>
2024-02-01 21:32:58 -0300

    Pagination Service

---
+++ Infrastructure/Interfaces/IPaginationService.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Interfaces
+{
+    public interface IPaginationService
+    {
+        Task<PagedResult<T>> PaginateAsync<T>(IQueryable<T> query, int pageNumber, int pageSize) where T : class;
+    }
+}
---
+++ Infrastructure/Utils/PaginationService.cs
@@ -0,0 +1,32 @@
+﻿using Infrastructure.Entities;
+using Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Utils
+{
+    public class PaginationService : IPaginationService
+    {
+        public async Task<PagedResult<T>> PaginateAsync<T>(IQueryable<T> query, int pageNumber, int pageSize) where T : class
+        {
+            if (pageNumber < 1) pageNumber = 1;
+            if (pageSize < 1) pageSize = 10;
+
+            var result = new PagedResult<T>
+            {
+                PageNumber = pageNumber,
+                PageSize = pageSize,
+                TotalCount = await query.CountAsync()
+            };
+
+            var skipAmount = (pageNumber - 1) * pageSize;
+            result.Items = await query.Skip(skipAmount).Take(pageSize).ToListAsync();
+
+            return result;
+        }
+    }
+}
