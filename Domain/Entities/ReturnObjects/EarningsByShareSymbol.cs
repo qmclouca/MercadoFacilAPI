e019d702aca1d13b4d356e39be5cb55e70813d69
qmclouca <qmclouca@gmail.com>
2024-02-05 21:53:45 -0300

    enpoints para a controller share

---
+++ Domain/Entities/ReturnObjects/EarningsByShareSymbol.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.Entities.ReturnObjects
+{
+    public class EarningsByShareSymbol
+    {
+        public EarningsByShareSymbol()
+        {
+        }
+        public EarningsByShareSymbol(string symbol, string companyName, double earningsPerShare)
+        {
+            this.symbol = symbol;
+            this.companyName = companyName;            
+            this.earningsPerShare = earningsPerShare;            
+        }
+        public string? symbol { get; set; }
+        public string? companyName { get; set; }
+        public double? earningsPerShare { get; set; }
+    }
+}
