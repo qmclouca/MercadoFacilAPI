75ad00655835dd3a306753e3d2e8672adf51ec99
qmclouca <qmclouca@gmail.com>
2024-02-06 23:51:29 -0300

    implementando autenticação e autorização

---
+++ Domain/Services/AuthService.cs
@@ -0,0 +1,57 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Domain.Interfaces.Services;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using System.Text;
+
+namespace Domain.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IRepository<User> _userRepository;
+        //private readonly IConfiguration _configuration;
+
+        public AuthService(IRepository<User> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public async Task<string> Authenticate(string email, string password)
+        {
+            var user = await _userRepository.GetAllQueryAsync(u => u.Email == email);
+
+            // Verifique se o usuário existe e se a senha está correta
+            // Supondo que você esteja usando um hash de senha e uma função para verificar isso
+            if (user == null || !VerifyPasswordHash(password, user.Password))
+            {
+                // Usuário não encontrado ou senha incorreta
+                return null;
+            }
+
+            // Se válido, gere o token JWT
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Key"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Role, user.Role ?? "User") // Use um valor padrão se o Role for null
+            }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+        private bool VerifyPasswordHash(string password, string storedHash)
+        {
+            // Implemente sua lógica de verificação de hash de senha aqui
+            // Por exemplo, se você estiver usando o BCrypt:
+            // return BCrypt.Net.BCrypt.Verify(password, storedHash);
+            return true; // Substitua isso pela verificação real do hash da senha
+        }
+    }
+}
--- MercadoFacilAPI/Program.cs
+++ MercadoFacilAPI/Program.cs
@@ -1,56 +1,81 @@
-    using CrossCutting.DependencyInjection;
-    using Data;
-    using Data.Repositories;
-    using Domain.Interfaces;
+using CrossCutting.DependencyInjection;
+using Data;
+using Data.Repositories;
+using Domain.Interfaces;
 using Infrastructure;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
-    var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-    builder.Services.AddDbContext<MercadoFacilDbContext>(options =>
-        options.UseSqlServer
-            (builder.Configuration.GetConnectionString("DefaultConnection")
-            , b => b.MigrationsAssembly("../Data")));
+builder.Services.AddDbContext<MercadoFacilDbContext>(options =>
+    options.UseSqlServer
+        (builder.Configuration.GetConnectionString("DefaultConnection")
+        , b => b.MigrationsAssembly("../Data")));
 
-    builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 #pragma warning disable CS8600, CS8603
-    builder.Services.AddScoped<IMercadoFacilDbContext>(provider => (IMercadoFacilDbContext)provider.GetService<MercadoFacilDbContext>());
+builder.Services.AddScoped<IMercadoFacilDbContext>(provider => (IMercadoFacilDbContext)provider.GetService<MercadoFacilDbContext>());
 #pragma warning restore CS8600, CS8603
-    builder.Services.AddApplicationServices();    
-    builder.Services.Configure<ExternalAPIConfigurations>(builder.Configuration.GetSection("ExternalAPIConfigurations"));
-    //builder.Services.AddTransient<IInfrastructureService, InfrastructureService>();
-    builder.Services.AddControllers();
-    builder.Services.AddHealthChecks();
-    builder.Services.AddCors(options =>
+builder.Services.AddApplicationServices();
+builder.Services.Configure<ExternalAPIConfigurations>(builder.Configuration.GetSection("ExternalAPIConfigurations"));
+//builder.Services.AddTransient<IInfrastructureService, InfrastructureService>();
+builder.Services.AddControllers();
+builder.Services.AddHealthChecks();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
     {
-        options.AddPolicy("AllowAll", builder =>
-        {
-            builder.AllowAnyOrigin()
-                .AllowAnyMethod()
-                .AllowAnyHeader();
-        });
+        builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
     });
-
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
-
-    var app = builder.Build();
-
-    if (app.Environment.IsDevelopment())
+});
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
     {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
 
-    app.UseCors("AllowAll");
-    app.UseHttpsRedirection();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseAuthentication();
+    app.UseAuthorization();
+}
+
+app.UseCors("AllowAll");
+app.UseHttpsRedirection();
 
 #if !DEBUG
     app.UseAuthentication();
     app.UseAuthorization();
 #endif
 
-    app.MapControllers();
-    app.MapHealthChecks("/health");
+app.MapControllers();
+app.MapHealthChecks("/health");
 
-    app.Run();
+app.Run();
